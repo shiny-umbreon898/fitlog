197a92d2334f075efce55fc1f2e25a9a1119753a
shiny-umbreon898 <cluehunter39ag@gmail.com>
2026-02-21 13:40:38 +0000

    setup hello world page and installation intstructions

--- backend/app/app.py
+++ backend/app/app.py
@@ -2,9 +2,17 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# Define a route for the root URL
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, World'
 
+
+@app.route('/dashboard')
+def dashboard():
+    return 'Dashboard Page'
+
+
+# Run the Flask app
 if __name__ == '__main__':
     app.run(debug=True)
