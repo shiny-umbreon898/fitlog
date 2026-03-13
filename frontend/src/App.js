0d9b48659cba47d359d20352be0bc83f67982da2
shiny-umbreon898 <cluehunter39ag@gmail.com>
2026-03-13 15:39:54 +0000

    linked registration and login to app ( needs testing)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,43 @@
+// default React app component from create-react-app
 import logo from './logo.svg';
 import './App.css';
 
+// React Router component for handling navigation
+import { BrowserRouter as Router, Routes, Route, Link, Switch } from "react-router-dom";
+
+// Page Components
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+
+// import Dashboard from "./pages/Dashboard";
+// import Profile from "./pages/Profile";
+
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+
+        <Router> // enable nav without page refresh
+
+            <div className="App">
+
+                <h1>Fitlog</h1>
+
+                <nav>
+                    <Link to="/register">Register</Link>
+                    <Link to="/login">Login</Link>
+                </nav>
+
+                <Routes>
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/login" element={<Login />} />
+                </Routes>
+
+            </div>
+
+        </Router>
+    );
+
 }
 
 export default App;
